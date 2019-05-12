68f64a951eed9e32a026ac261a099896ac7a3cba
gchronos <adquasarium@gmail.com>
2019-05-12 17:30:23 +0300

    fix path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-	{
-		path: '',
-		redirectTo: 'queues',
-		pathMatch: 'full'
-	},
-	{path: 'queues', loadChildren: './pages/queues/queues.module#QueuesPageModule'},
+	{path: '', loadChildren: './pages/queues/queues.module#QueuesPageModule'},
 	{path: 'queue/:id', loadChildren: './pages/queue/queue.module#QueuePageModule'},
 	{path: 'history', loadChildren: './pages/history/history.module#HistoryPageModule'}
 ];
