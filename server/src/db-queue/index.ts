d3892d0fb2987b6a504e88e23cba5fbc6403b3c5
gchronos <adquasarium@gmail.com>
2019-05-15 16:29:30 +0300

    server

---
+++ server/src/db-queue/index.ts
@@ -0,0 +1,40 @@
+import { DocumentData } from '@google-cloud/firestore';
+import { IQueueItem } from './index.models';
+import admin from 'firebase-admin/lib/index';
+const serviceAccount = require('../../serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const StorageKey = 'queues';
+const StorageCollectionKey = 'list';
+
+export function addQueue(userId: string, queueItem: IQueueItem) {
+    const queueType = queueItem.queueType;
+    const queueRef = admin.firestore()
+        .collection('users')
+        .doc(userId)
+        .collection(StorageKey)
+        .doc(queueType);
+
+    // push to collection new queueItem
+    queueRef.collection(StorageCollectionKey).add(queueItem);
+
+    // increment videoQueueLen
+    const increment = admin.firestore.FieldValue.increment(1);
+    queueRef.set({
+        videoQueueLen: increment,
+        queueType
+    }, { merge: true });
+}
+
+export async function getUsers(): Promise<DocumentData[]> {
+    const docs = await admin.firestore().collection('users').listDocuments();
+
+    return Promise.all(docs.map(async user => {
+        const userData = await user.get();
+
+        return userData.data();
+    }));
+}
---
+++ server/src/views/init-socket.ts
@@ -0,0 +1,61 @@
+import { addQueue } from '../db-queue';
+import { toYouTubeEmbedded } from '../utils/urlParser';
+import { DonationAlertsMessage } from './init-socket.models';
+import { IQueueItem } from '../db-queue/index.models';
+const io = require('socket.io-client');
+
+
+
+export class DonationAlertsSocket {
+    constructor(userId: string, token: string) {
+        const socket = io.connect('wss://socket.donationalerts.ru:443', {
+            reconnection: true,
+            reconnectionDelayMax: 5000,
+            reconnectionDelay: 1000,
+        });
+
+        socket.on('connect', () => {
+            socket.emit('add-user', {token: token, type: 'alert_widget'});
+        });
+
+        let counter = 0;
+        socket.on('donation', (msg: any) => {
+            const newDonation: DonationAlertsMessage = JSON.parse(msg);
+            if (newDonation) {
+                // TODO: remove after tests
+                counter++;
+                newDonation.amount = counter;
+                newDonation.message = '[https://youtu.be/oFElsHvWxn0?t=6058, x2] some text';
+
+                let videoStr = newDonation.message.match(/\[([^)]+)\]/gm)[0];
+
+                if (videoStr) {
+                    // remove brackets
+                    videoStr = videoStr.replace(/[\[\]']+/g, '');
+
+                    const videoArr = videoStr.split(',');
+                    const youtubeUrl = videoArr[0];
+                    const queueType = videoArr[1].trim();
+
+                    // convert youtube link tu embedded link
+                    const url = toYouTubeEmbedded(youtubeUrl);
+
+                    const newVideoObj: IQueueItem = {
+                        id: +new Date(),
+                        message: newDonation.message,
+                        username: newDonation.username,
+                        amount: newDonation.amount,
+                        currency: newDonation.currency,
+                        date_created: newDonation.date_created,
+                        url,
+                        queueType
+                    };
+
+                    console.log(newVideoObj);
+
+                    addQueue(userId, newVideoObj);
+                }
+            }
+        });
+    }
+}
